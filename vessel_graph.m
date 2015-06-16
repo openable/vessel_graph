82afe4750c66a4820670c0b272d0d76e0854c37e
Yong-il Lee <lyikorea@kaist.edu>
2015-06-16 15:41:26 +0900

    - commit test

--- vessel_graph.m
+++ vessel_graph.m
@@ -28,6 +28,7 @@ function vessel_graph
 %     handling the ButtonDownFcn callback.
 %     Similarly you could create a second listbox to display the edges
 %     and handle the assignment of values in the same manner... I will that part to you :)
+%   branch test
 
     % data
     showVertices = false;   % flag to determine whether to show node labels
