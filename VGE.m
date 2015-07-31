5f08be5734cb8c19ab849a923a07ba53328120b2
Yong-il Lee <lyikorea@kaist.edu>
2015-07-31 13:46:07 +0900

    [기능 변경] - STL reader 함수 교체, 동작 안함.ㅜ

--- VGE.m
+++ VGE.m
@@ -696,179 +696,35 @@ h = initGUI();
 
 
     function onOpen3D(~,~)
-        filename = uigetfile('*.stl','가져올 3D 모델 파일(.stl)을 선택하세요.');
-        
-        [F, V, C] = rndread(filename);
-        p = patch('faces', F, 'vertices' ,V);
-        %set(p, 'facec', 'b');              % Set the face color (force it)
-        set(p, 'facec', 'flat');            % Set the face color flat
-        set(p, 'FaceVertexCData', C);       % Set the color (from file)
-        %set(p, 'facealpha',.4)             % Use for transparency
-        set(p, 'EdgeColor','none');         % Set the edge color
-        %set(p, 'EdgeColor',[1 0 0 ]);      % Use to see triangles, if needed.
         set(h.ax3D.XLabel, 'String', 'X');
         set(h.ax3D.YLabel, 'String', 'Y');
         set(h.ax3D.ZLabel, 'String', 'Z');
         
-        light                               % add a default light
-        daspect([1 1 1])                    % Setting the aspect ratio
-        view(3)                             % Isometric view
-        %xlabel('X'),ylabel('Y'),zlabel('Z')
-        drawnow                             %, axis manual
-        %
-        %disp(['CAD file ' filename ' data is read, will now show object rotating'])
-
-    end
-
-
-    function Rx = rx(THETA)
-        % ROTATION ABOUT THE X-axis
-        %
-        % Rx = rx(THETA)
-        %
-        % This is the homogeneous transformation for
-        % rotation about the X-axis.
-        %
-        %	    NOTE:  The angle THETA must be in DEGREES.
-        %
-        THETA = THETA*pi/180;  % Note: THETA in radians.
-        c = cos(THETA);
-        s = sin(THETA);
-        Rx = [1 0 0 0; 0 c -s 0; 0 s c 0; 0 0 0 1];
-    end
-
-
-    function Ry = ry(THETA)
-        % ROTATION ABOUT THE Y-axis
-        %
-        % Ry = ry(THETA)
-        %
-        % This is the homogeneous transformation for
-        % rotation about the Y-axis.
-        %
-        %		NOTE: The angel THETA must be in DEGREES.
-        %
-        THETA = THETA*pi/180;  %Note: THETA is in radians.
-        c = cos(THETA);
-        s = sin(THETA);
-        Ry = [c 0 s 0; 0 1 0 0; -s 0 c 0; 0 0 0 1];
-    end
-
-    function Rz = rz(THETA)
-        % ROTATION ABOUT THE Z-axis
-        %
-        % Rz = rz(THETA)
-        %
-        % This is the homogeneous transformation for
-        % rotation about the Z-axis.
-        %
-        %		NOTE:  The angle THETA must be in DEGREES.
-        %
-        THETA = THETA*pi/180;  %Note: THETA is in radians.
-        c = cos(THETA);
-        s = sin(THETA);
-        Rz = [c -s 0 0; s c 0 0; 0 0 1 0; 0 0 0 1];
-    end
-
-    function T = tl(x,y,z)
-        % TRANSLATION ALONG THE X, Y, AND Z AXES
-        %
-        % T = tl(x,y,z)
-        %
-        % This is the homogeneous transformation for
-        % translation along the X, Y, and Z axes.
-        %
-        T = [1 0 0 x; 0 1 0 y; 0 0 1 z; 0 0 0 1];
-    end
-
-
-    function vsize = maxv(V)
-        %
-        % Look at the xyz elements of V, and determine the maximum
-        % values during some simple rotations.
-        vsize = max(max(V));
-        % Rotate it a bit, and check for max and min vertex for viewing.
-        for ang = 0:10:360
-            vsizex = rx(ang)*V;
-            maxv = max(max(vsizex));
-            if maxv > vsize, vsize = maxv; end
-            vsizey = ry(ang)*V;
-            maxv = max(max(vsizey));
-            if maxv > vsize, vsize = maxv; end
-            vsizez = rz(ang)*V;
-            maxv = max(max(vsizez));
-            if maxv > vsize, vsize = maxv; end
-            vsizev = rx(ang)*ry(ang)*rz(ang)*V;
-            maxv = max(max(vsizev));
-            if maxv > vsize, vsize = maxv; end
-        end
-    end
-
-    function [fout, vout, cout] = rndread(filename)
-        % Reads CAD STL ASCII files, which most CAD programs can export.
-        % Used to create Matlab patches of CAD 3D data.
-        % Returns a vertex list and face list, for Matlab patch command.
+        filename = uigetfile('*.stl','가져올 3D 모델 파일(.stl)을 선택하세요.');
         
-        fid=fopen(filename, 'r'); %Open the file, assumes STL ASCII format.
-        if fid == -1
-            error('File could not be opened, check name or path.')
-        end
-        %
-        % Render files take the form:
-        %
-        %solid BLOCK
-        %  color 1.000 1.000 1.000
-        %  facet
-        %      normal 0.000000e+00 0.000000e+00 -1.000000e+00
-        %      normal 0.000000e+00 0.000000e+00 -1.000000e+00
-        %      normal 0.000000e+00 0.000000e+00 -1.000000e+00
-        %    outer loop
-        %      vertex 5.000000e-01 -5.000000e-01 -5.000000e-01
-        %      vertex -5.000000e-01 -5.000000e-01 -5.000000e-01
-        %      vertex -5.000000e-01 5.000000e-01 -5.000000e-01
-        %    endloop
-        % endfacet
-        %
-        % The first line is object name, then comes multiple facet and vertex lines.
-        % A color specifier is next, followed by those faces of that color, until
-        % next color line.
-        %
-        CAD_object_name = sscanf(fgetl(fid), '%*s %s');  %CAD object name, if needed.
-        %                                                %Some STLs have it, some don't.
-        vnum=0;       %Vertex number counter.
-        report_num=0; %Report the status as we go.
-        VColor = 0;
-        %
-        while feof(fid) == 0                    % test for end of file, if not then do stuff
-            tline = fgetl(fid);                 % reads a line of data from file.
-            fword = sscanf(tline, '%s ');       % make the line a character string
-            % Check for color
-            if strncmpi(fword, 'c',1) == 1;    % Checking if a "C"olor line, as "C" is 1st char.
-                VColor = sscanf(tline, '%*s %f %f %f'); % & if a C, get the RGB color data of the face.
-            end                                % Keep this color, until the next color is used.
-            if strncmpi(fword, 'v',1) == 1;    % Checking if a "V"ertex line, as "V" is 1st char.
-                vnum = vnum + 1;                % If a V we count the # of V's
-                report_num = report_num + 1;    % Report a counter, so long files show status
-                if report_num > 249;
-                    disp(sprintf('Reading vertix num: %d.',vnum));
-                    report_num = 0;
-                end
-                v(:,vnum) = sscanf(tline, '%*s %f %f %f'); % & if a V, get the XYZ data of it.
-                c(:,vnum) = VColor;              % A color for each vertex, which will color the faces.
-            end                                 % we "*s" skip the name "color" and get the data.
-        end
-        %   Build face list; The vertices are in order, so just number them.
-        %
-        fnum = vnum/3;      %Number of faces, vnum is number of vertices.  STL is triangles.
-        flist = 1:vnum;     %Face list of vertices, all in order.
-        F = reshape(flist, 3,fnum); %Make a "3 by fnum" matrix of face list data.
-        %
-        %   Return the faces and vertexs.
-        %
-        fout = F';  %Orients the array for direct use in patch.
-        vout = v';  % "
-        cout = c';
-        %
-        fclose(fid);
+        fv = stlread(filename);
+        patch(fv,'FaceColor',       [0.8 0.8 1.0], ...
+                 'EdgeColor',       'none',        ...
+                 'FaceLighting',    'gouraud',     ...
+                 'AmbientStrength', 0.15);
+
+        % Add a camera light, and tone down the specular highlighting
+        camlight('headlight');
+        material('dull');
+
+        % Fix the axes scaling, and set a nice view angle
+        axis('image');
+        view([-135 35]);
+
+        
+%         light                               % add a default light
+%         daspect([1 1 1])                    % Setting the aspect ratio
+%         view(3)                             % Isometric view
+%         %xlabel('X'),ylabel('Y'),zlabel('Z')
+%         drawnow                             %, axis manual
+%         %
+%         %disp(['CAD file ' filename ' data is read, will now show object rotating'])
+
     end
+
 end
