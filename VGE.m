6627043588c331df81a511f3801a510a18e670e5
Yong-il Lee <link@openable.net>
2015-08-18 00:15:52 +0900

    [기능 추가] - 데이터 커서 모드에서 data tip text 내용 없앰. 나중에 꼭지점 이름 순차적으로 해볼까 하나 지금은 안 함.

--- VGE.m
+++ VGE.m
@@ -772,6 +772,7 @@ redraw();
 
     function onCursor3D(~,~)
         dcm = datacursormode(h.fig);
+        set(dcm,'UpdateFcn',@dataText)
 
         if strcmp(get(dcm, 'enable'), 'on')
             set(dcm, 'enable', 'off');
@@ -989,5 +990,23 @@ end
             redraw3D();
         end
     end
+
+function output_txt = dataText(obj,event_obj)
+% Display the position of the data cursor
+% obj          Currently not used (empty)
+% event_obj    Handle to event object
+% output_txt   Data cursor text string (string or cell array of strings).
+
+% pos = get(event_obj,'Position');
+% output_txt = {['X: ',num2str(pos(1),4)],...
+%     ['Y: ',num2str(pos(2),4)]};
+% 
+% % If there is a Z-coordinate in the position, display it as well
+% if length(pos) > 2
+%     output_txt{end+1} = ['Z: ',num2str(pos(3),4)];
+% end
+output_txt = '';
+
+end
 
 end
