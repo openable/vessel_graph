b59c9fbd92f30fa8aa3beba611206caaf2b079aa
Yong-il Lee <link@openable.net>
2015-08-25 05:19:57 +0900

    [기능 삭제] - 데이터 팁 가져오기 / h.importTip3D / onImportTip3D - 데이터 팁 내보내기 / h.exportTip3D / onExportTip3D - 그래프 점 > 데이터 팁 / h.setTips / onSetTips - h.areaData 패널
    [기능 변경]
    -  데이터 팁> 그래프 점 (onSetVertices) 함수에 기존 그래프 점이 있으면 데이터 팁을 추가하는 기능으로 동작하게 함. (이전 버전에는 무조건 데이터 팁으로 그래프 점을 그대로 옮겼음)

--- VGE.m
+++ VGE.m
@@ -135,33 +135,25 @@ redraw();
             'Position',[90 800 60 20],'Callback',@onVein3D);
         h.list3D = uicontrol('Style','listbox', 'Parent',h.tab2, 'String',{}, ...
             'Min',1, 'Max',1, 'Value',-1, 'FontName', 'Fixedsys', 'FontSize', 10, ...
-            'Position',[20 410 130 380], 'Callback',@onSelect3D); % 140
+            'Position',[20 320 130 470], 'Callback',@onSelect3D); % 140
         h.labelText3D = uicontrol('Style','text', 'Parent',h.tab2, 'String',{}, ...
             'String', '이름:', 'HorizontalAlignment', 'left', 'FontSize', 10, ...
-            'Position',[20 380 40 20]);
+            'Position',[20 290 40 20]);
         h.labelEdit3D = uicontrol('Style','edit', 'Parent',h.tab2, 'String',{}, ...
             'HorizontalAlignment', 'left', 'Enable', 'off', ...
-            'Position',[60 380 60 20], 'KeyPressFcn',@onEditKey3D);
+            'Position',[60 290 60 20], 'KeyPressFcn',@onEditKey3D);
         h.labelSet3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','설정', ...
-            'Position',[125 380 25 20], 'Callback',@onLabelSet3D, 'Enable', 'off', 'KeyPressFcn',@onSetKey3D);
+            'Position',[125 290 25 20], 'Callback',@onLabelSet3D, 'Enable', 'off', 'KeyPressFcn',@onSetKey3D);
         
-        h.area3D = uipanel('Parent', h.tab2, 'Title', '', 'Units', 'pixels', 'Position', [15 285 140 90]);
+        h.area3D = uipanel('Parent', h.tab2, 'Title', '', 'Units', 'pixels', 'Position', [15 165 140 120]);
         h.open3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','3D 모델 불러오기', ...
-            'Position',[20 350 130 20], 'Callback',@onOpen3D, 'Enable', 'on');
+            'Position',[20 260 130 20], 'Callback',@onOpen3D, 'Enable', 'on');
         h.hide3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','3D 모델 감추기/보이기', ...
-            'Position',[20 320 130 20], 'Callback',@onHide3D, 'Enable', 'on');
+            'Position',[20 230 130 20], 'Callback',@onHide3D, 'Enable', 'on');
         h.clearModel3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','3D 모델 초기화', ...
-            'Position',[20 290 130 20], 'Callback',@onClearModel3D, 'Enable', 'on');
-        
-        h.areaData = uipanel('Parent', h.tab2, 'Title', '', 'Units', 'pixels', 'Position', [15 165 140 120]);
+            'Position',[20 200 130 20], 'Callback',@onClearModel3D, 'Enable', 'on');
         h.cursor3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','데이터 커서 모드', ...
-            'Position',[20 260 130 20], 'Callback',@onCursor3D, 'Enable', 'on');
-        h.importTip3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','데이터 팁 가져오기', ...
-            'Position',[20 230 130 20], 'Callback',@onImportTip3D, 'Enable', 'on');
-        h.exportTip3D = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','데이터 팁 내보내기', ...
-            'Position',[20 200 130 20], 'Callback',@onExportTip3D, 'Enable', 'on');
-        h.setTips = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','그래프 점 > 데이터 팁', ...
-            'Position',[20 170 130 20], 'Callback',@onSetTips, 'Enable', 'on');
+            'Position',[20 170 130 20], 'Callback',@onCursor3D, 'Enable', 'on');
         
         h.areaGraph = uipanel('Parent', h.tab2, 'Title', '', 'Units', 'pixels', 'Position', [15 15 140 150]);
         h.setVertices = uicontrol('Style','pushbutton', 'Parent',h.tab2, 'String','데이터 팁 > 그래프 점', ...
@@ -799,20 +791,20 @@ redraw();
     function onSetVertices(~,~)
         dcm = datacursormode(h.fig);
         data3 = getCursorInfo(dcm);
-%         if isempty(ptsAtery3D)
+        if isempty(ptsAtery3D)
             ptsAtery3D = zeros(0,3);
             adjAtery3D = sparse([]);
             for n = 1:size(data3,2);
                 ptsAtery3D(n,:) = data3(n).Position;
                 adjAtery3D(n,n) = 0;
             end
-%         else
-%             listN = size(ptsAtery3D,1);
-%             for n = 1:size(data3,2);
-%                 ptsAtery3D(listN+n,:) = data3(n).Position;
-%                 adjAtery3D(listN+n,listN+n) = 0;
-%             end
-%         end
+        else
+            listN = size(ptsAtery3D,1);
+            for n = 1:size(data3,2);
+                ptsAtery3D(listN+n,:) = data3(n).Position;
+                adjAtery3D(listN+n,listN+n) = 0;
+            end
+        end
         
         set(h.p3DH, 'HitTest','off');
         redraw3D();
