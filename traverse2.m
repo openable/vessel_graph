c72c4ab09b2f060f494e6bb509e4b40cf2470217
Yong-il Lee <link@openable.net>
2015-11-10 04:27:00 +0900

    [기능 추가] - 분기 일어날 때 마다 Depth 값 증가 할당
    [리펙토링]
    - 설명 주석 추가

--- traverse2.m
+++ traverse2.m
@@ -35,6 +35,7 @@ while ~path.isempty()
             fTable{eIndex,2} = eNode;
             fTable{eIndex,3} = eIndex;
             
+            % 시작점이 끝나는 점이었던 선분을 찾아 부모 선분 번호 할당
             for m = 1:size(fTable,1)
                 if fTable{m,2} == sNode
                     fTable{eIndex,4} = fTable{m,3};
@@ -42,6 +43,7 @@ while ~path.isempty()
                 end
             end
             
+            % 분기 갯수를 부모는 1 빼고 할당
             branch = find(adjM(eNode,:) == 1);
             fTable{eIndex,7} = length(branch)-1;
         end
@@ -49,10 +51,23 @@ while ~path.isempty()
 end
 
 for n = 1:size(fTable,1)
+    % 부모 선분가 백처 차이 계산
     if ~isempty(fTable{n,4})
         cVector = pts(fTable{n,2},:) - pts(fTable{n,1},:);
         pVector = pts(fTable{fTable{n,4},2},:) - pts(fTable{fTable{n,4},1},:);
         fTable{n,5} = cVector - pVector;
+    end
+    
+    % 부모 선분에서 여러 선분 분기 여부에 따라 depth 값 증가, 1개면 depth 계속 상속
+    if isempty(fTable{n,4})
+        fTable{n,6} = 1;    % root의 선분은 depth 1로 설정
+    else
+        pDepth = fTable{fTable{n,4},6};
+        if fTable{fTable{n,4},7} > 1
+            fTable{n,6} = pDepth+1;
+        else
+            fTable{n,6} = pDepth;
+        end
     end
 end
 
