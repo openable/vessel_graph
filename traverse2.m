3f842ba91f4245471d46f0855d506c0961e4b43e
Yong-il Lee <link@openable.net>
2015-11-10 02:45:16 +0900

    [기능 추가] - sNode / eNode / current edge index 정보 출력, parents edge index / 각도 벡터 / depth / 분기 갯수는 아직 미출력

--- traverse2.m
+++ traverse2.m
@@ -2,15 +2,40 @@ function traverse2(root, adjM)
 parent = CList();
 path = CQueue();
 path.push(root);
+edgeCount = 0;
 
 while ~path.isempty()
     node = path.pop();
-    disp(node);
     parent.pushtorear(node);
     children = find(adjM(node,:) == 1);
     for n = 1:length(children)
        if ~parent.contains(children(n));
           path.push(children(n));
+          edgeCount = edgeCount + 1;
        end
     end
 end
+
+
+parent.removeall();
+fTable = cell(edgeCount, 7);    %sNode / eNode / current edge index / parents edge index / °¢µµ º¤ÅÍ / depth / ºÐ±â °¹¼ö
+path.push(root);
+eIndex = 0;
+
+while ~path.isempty()
+    sNode = path.pop();
+    parent.pushtorear(sNode);
+    children = find(adjM(sNode,:) == 1);
+    for n = 1:length(children)
+        eNode = children(n);
+        if ~parent.contains(eNode);
+            path.push(eNode);
+            eIndex = eIndex + 1;
+            fTable{eIndex,1} = sNode;
+            fTable{eIndex,2} = eNode;
+            fTable{eIndex,3} = eIndex;
+        end
+    end
+end
+
+disp(fTable);
