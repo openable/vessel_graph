9ac83e39068ff4d3f6a4382ca993ea780a2b2728
Yong-il Lee <link@openable.net>
2015-11-11 04:13:08 +0900

    [기능 변경] - fTable 5열, 차이 벡터 구하던 것을 각도로 변경

--- traverse2.m
+++ traverse2.m
@@ -51,11 +51,17 @@ while ~path.isempty()
 end
 
 for n = 1:size(fTable,1)
-    % 부모 선분가 백처 차이 계산
+    % 부모 선분과 현재 선분 사이 각도 계산
     if ~isempty(fTable{n,4})
-        cVector = pts(fTable{n,2},:) - pts(fTable{n,1},:);
-        pVector = pts(fTable{fTable{n,4},2},:) - pts(fTable{fTable{n,4},1},:);
-        fTable{n,5} = cVector - pVector;
+        %현재 선분의 시작점을 중심으로 각도 측정
+        pVector = pts(fTable{fTable{n,4},1},:) - pts(fTable{fTable{n,4},2},:); %시작점에서 끝나는 점 뺌
+        cVector = pts(fTable{n,2},:) - pts(fTable{n,1},:); %끝나는 점에서 시작점 뺌
+        
+        pUnit = pVector / norm(pVector);
+        cUnit = cVector / norm(cVector);
+        theta = acos(dot(pUnit, cUnit));
+        
+        fTable{n,5} = theta*180/pi;
     end
     
     % 부모 선분에서 여러 선분 분기 여부에 따라 depth 값 증가, 1개면 depth 계속 상속
